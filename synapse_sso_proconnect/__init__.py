a3cc2ee00c196513bda2a025301101ff18e8b47d
olivier <odelcroi@gmail.com>
2024-12-17 16:20:08 +0100

    update log

--- synapse_sso_proconnect/__init__.py
+++ synapse_sso_proconnect/__init__.py
@@ -41,17 +41,20 @@ class LoginCallback(object):
         # This mapper is only invoked when the oidc user is not mapped to a MxId
         # After the first login the oidc user unique field (sub) is mapped to a MxId
         # Thus the 3PID substitution happens only at the first login of the oidc user
-        sso_extra_attributes = self.module_api._auth_handler._extra_attributes.get(user_id, None)
-        logger.info("extra attributes found for user %s : %s",user_id, sso_extra_attributes or "nothing")
 
-        if sso_extra_attributes:
-            #check if extra attributes were attached to user_id 
-            extra_attributes = sso_extra_attributes.extra_attributes
-            oidc_email = extra_attributes.get('oidc_email', None)
-            current_threepid_email = extra_attributes.get('current_threepid_email', None)
-            if(current_threepid_email and oidc_email and current_threepid_email != oidc_email):
-                # make the substitution
-                await self.substitute_threepid(user_id, current_threepid_email, oidc_email)
+        # Check if extra attributes were attached to user_id 
+        sso_extra_attributes = self.module_api._auth_handler._extra_attributes.get(user_id, None)
+        logger.info("extra attributes found for user %s : %s",user_id, sso_extra_attributes  or "None")
+
+        if not sso_extra_attributes:
+            return
+
+        extra_attributes = sso_extra_attributes.extra_attributes
+        oidc_email = extra_attributes.get('oidc_email', None)
+        current_threepid_email = extra_attributes.get('current_threepid_email', None)
+        if(current_threepid_email and oidc_email and current_threepid_email != oidc_email):
+            # make the substitution
+            await self.substitute_threepid(user_id, current_threepid_email, oidc_email)
 
     async def substitute_threepid(self,user_id, current_threepid_email, new_threepid_email) -> None:
         """
